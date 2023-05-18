82b7d4938583b30d32d10320b7a75a6e923597db
John Vu <johntrinhvu@yahoo.com>
2023-05-17 17:17:51 -0700

    Add exercise model and routine model

---
+++ models/routine.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+
+const routineSchema = new Schema({
+  title: { type: String, required: true },
+  
+});
+
+// Compile the schema into a model and export it
+module.exports = mongoose.model('Routine', routineSchema);
